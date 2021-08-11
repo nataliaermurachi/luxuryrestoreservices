89cda2e1dfc12566ea00ab27ad34f5825a9ec062
Natalia Ermurachi <ermurachinatalia5@gmail.com>
2021-08-11 17:24:41 +0200

    finished services section

---
+++ public/js/init.js
@@ -0,0 +1,29 @@
+// carousel
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems, {
+        numVisible: 9
+    });
+});
+
+// modal
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems);
+});
+
+// paralax
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems);
+});
+
+// feature
+$(document).ready(function() {
+    $('.tap-target').tapTarget();
+});
+
+
+$('#open').click(function() {
+    $('.tap-target').tapTarget('open')
+});
