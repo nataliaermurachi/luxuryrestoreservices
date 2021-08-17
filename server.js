bef88204aec77887106a33fb4c0f7319ac44a162
Natalia Ermurachi <ermurachinatalia5@gmail.com>
2021-08-17 11:15:25 +0200

    add express and ejs,integrate contact form and use send grid

---
+++ server.js
@@ -0,0 +1,40 @@
+const path = require('path')
+
+const express = require('express');
+const app = express();
+
+const sendEmail = require('./utils/sendemail');
+
+app.use(express.urlencoded({ extended: false }));
+app.use('/public', express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('main')
+});
+
+app.get('/sent', (req, res) => {
+    res.render('sent')
+});
+
+app.post('/sendemail', (req, res) => {
+    const { fname, phone, email, message } = req.body;
+    const from = 'ermurachinatalia5@gmail.com'; // sender from the sendgrid account
+    const to = 'ermurachinatalia5@gmail.com';
+    const subject = "New contact request";
+    const output = `
+      <p>You have a new Contact Request</p>
+      <h3>Contact Details</h3>
+      <ul>
+        <li>Name: ${fname}</li>
+        <li>Name: ${phone}</li>
+        <li>Email: ${email}</li>
+        <li>Message: ${message}</li>
+      </ul>
+  `;
+    sendEmail(to, from, subject, output);
+    res.redirect('/sent')
+})
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running op port: ${PORT}`));
